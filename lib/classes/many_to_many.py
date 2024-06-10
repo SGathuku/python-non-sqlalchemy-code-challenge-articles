f50fd80a14bb94cc14c690e8423625ceb2f05abf
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-10 14:13:30 +0300

    Testing class Author in many_to_many.py

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -35,22 +35,37 @@ class Article:
         if not isinstance(magazine, Magazine):
             raise ValueError("Magazine must be of type Magazine")
         self._magazine = magazine
-        
+
+       
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("Name must be a non-empty string")
+        self._name = name
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
+        @name.setter
+        def name(self, name):
+            if not isinstance(name, str) or len(name) == 0:
+                raise ValueError("Name must be a non-empty string")
+            self._name = name
 
     def articles(self):
-        pass
+        return self._articles
 
     def magazines(self):
-        pass
+        return list(set(article.magazine for article in self._articles))
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        if not self._articles:
+            return None
+        return list(set(article.magazine.category for article in self._articles))
 
 class Magazine:
     def __init__(self, name, category):

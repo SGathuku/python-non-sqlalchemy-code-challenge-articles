cdcd82b3b1cf1a0bee59686ee8981013957c41e7
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-10 14:10:31 +0300

    Testing class Article in many_to_many.py

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,8 +1,40 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise ValueError("Title must be a string and between 5 and 50 characters")
+        self._title = title
+
+        self._author = author
+        self._magazine = magazine
+        self._author.articles().append(self)
+        self._magazine.articles().append(self)
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, author):
+        if not isinstance(author, Author):
+            raise ValueError("Author must be of type Author")
+        self._author = author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, magazine):
+        if not isinstance(magazine, Magazine):
+            raise ValueError("Magazine must be of type Magazine")
+        self._magazine = magazine
         
 class Author:
     def __init__(self, name):
